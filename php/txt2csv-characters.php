2574dd4f6d1fbc184c90cdb1e76bbbec04ff7e46
conmaol <markmcco@gmail.com>
2024-08-23 10:01:55 +0100

    stuff

--- php/txt2csv-characters.php
+++ php/txt2csv-characters.php
@@ -2,18 +2,19 @@
 
 $chars = mb_str_split(file_get_contents($argv[1]));
 
-echo 'from,to,label,value,case,accent' . PHP_EOL;
+//echo 'from,to,label,value,case,accent' . PHP_EOL;
 
 foreach ($chars as $key => $val) {
-  echo 'ip_1_' . $key . ',' . 'ip_1_' . $key+1 . ',';
+  
   
   if (ctype_digit($val)) {
-    echo 'DIGIT,' . $val . ',,';
+    //echo 'DIGIT,' . $val . ',,';
   }
   else if (ctype_punct($val) || $val=='’' || $val=='‘' || $val=='“' || $val=='”') {
-    echo 'PUNCTUATION,"' . $val . '",,';
+    //echo 'PUNCTUATION,"' . $val . '",,';
   }
   else if ($val==' ' || $val==PHP_EOL) {
+    /*
     echo 'SPACE,';
     if ($val==PHP_EOL) {
       echo 'vertical,,';
@@ -21,47 +22,50 @@ foreach ($chars as $key => $val) {
     else {
       echo 'horizontal,,';
     }
+    */
   }
   else if (ctype_lower($val)) {
-    echo 'ALPHABETIC,' . strtoupper($val) . ',lower,none';
+    //echo 'ALPHABETIC,' . strtoupper($val) . ',lower,none';
   }
   else if (ctype_upper($val)) {
-    echo 'ALPHABETIC,' . $val . ',upper,none';
+    //echo 'ALPHABETIC,' . $val . ',upper,none';
   }
   else if ($val=='à') {
-    echo 'ALPHABETIC,A,lower,grave';
+    //echo 'ALPHABETIC,A,lower,grave';
   }
   else if ($val=='è') {
-    echo 'ALPHABETIC,E,lower,grave';
+    //echo 'ALPHABETIC,E,lower,grave';
   }
   else if ($val=='ì') {
-    echo 'ALPHABETIC,I,lower,grave';
+    //echo 'ALPHABETIC,I,lower,grave';
   }
   else if ($val=='ò') {
-    echo 'ALPHABETIC,O,lower,grave';
+    //echo 'ALPHABETIC,O,lower,grave';
   }
   else if ($val=='ù') {
-    echo 'ALPHABETIC,U,lower,grave';
+    //echo 'ALPHABETIC,U,lower,grave';
   }
   else if ($val=='À') {
-    echo 'ALPHABETIC,A,upper,grave';
+    //echo 'ALPHABETIC,A,upper,grave';
   }
   else if ($val=='È') {
-    echo 'ALPHABETIC,E,upper,grave';
+    //echo 'ALPHABETIC,E,upper,grave';
   }
   else if ($val=='Ì') {
-    echo 'ALPHABETIC,I,upper,grave';
+    //echo 'ALPHABETIC,I,upper,grave';
   }
   else if ($val=='Ò') {
-    echo 'ALPHABETIC,O,upper,grave';
+    //echo 'ALPHABETIC,O,upper,grave';
   }
   else if ($val=='Ù') {
-    echo 'ALPHABETIC,U,upper,grave';
+    //echo 'ALPHABETIC,U,upper,grave';
   }
   else {
+    echo 'ip_1_' . $key . ',' . 'ip_1_' . $key+1 . ',';
     echo 'CHARACTER,' . $val . ',,';
+    echo PHP_EOL;
   }
-  echo PHP_EOL;
+  
 }
 
 ?>
