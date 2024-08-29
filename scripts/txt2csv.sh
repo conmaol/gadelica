14ec80dc0d63ee3662d7b1fbb417a6476726f29b
conmaol <markmcco@gmail.com>
2024-08-29 19:21:55 +0100

    refresh

--- scripts/txt2csv.sh
+++ scripts/txt2csv.sh
@@ -5,9 +5,9 @@
 
 mkdir ../corpus/$1/csv/
 php ../php/txt2csv-nodes.php $1 > ../corpus/$1/csv/nodes.csv               # node ids
-php ../php/txt2csv-alphabetic.php $1 > ../corpus/$1/csv/alphabetic.csv.    # labelled edges representing alphabetic characters
+php ../php/txt2csv-alphabetic.php $1 > ../corpus/$1/csv/alphabetic.csv    # labelled edges representing alphabetic characters
 php ../php/txt2csv-digits.php $1 > ../corpus/$1/csv/digits.csv             # labelled edges representing digits
-php ../php/txt2csv-punctuation.php $1 > ../corpus/$1/csv/punctuation.csv.  # labelled edges representing punctuation markers
+php ../php/txt2csv-punctuation.php $1 > ../corpus/$1/csv/punctuation.csv  # labelled edges representing punctuation markers
 php ../php/txt2csv-spaces.php $1 > ../corpus/$1/csv/spaces.csv             # labelled edges representing horizontal and vertical whitespace
 php ../php/txt2csv-words.php $1 > ../corpus/$1/csv/words.csv               # labelled edges representing orthographic words
 php ../php/txt2csv-other.php $1                                            # identifies any characters missed or overlooked in the above scripts
